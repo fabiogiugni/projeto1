df1334e24a344291065a62af1ec355e1119626ff
Θαλησ <104565512+Thalesed@users.noreply.github.com>
2025-10-24 00:51:03 +0000

    fix: Classes Group Company Team Department

--- backend/model/entities/company.py
+++ backend/model/entities/company.py
@@ -1,16 +1,20 @@
 from group import Group
 
 class Company(Group):
-    def __init__(self, id : int, name : str, cnpj : str):
-        super(id, name)
+    def __init__(self, id : str, name : str, cnpj : str):
+        super().__init__(id, name)
         self.__cnpj = cnpj
         self.__departmentIDs = []
 
-    def addDepartment(self, departmentID : int):
+    def addDepartment(self, departmentID : str):
         self.__departmentIDs.append(departmentID)
 
-    def removeDepartment(self, departmentID : int):
+    def removeDepartment(self, departmentID : str):
         self.__departmentIDs.remove(departmentID)
+
+    @property
+    def departmentIDs(self):
+        return self.__departmentIDs
 
     @property
     def cnpj(self):
