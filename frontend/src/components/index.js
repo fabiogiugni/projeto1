0a40afd29df8976dc22240b1253402e039d2d7be
SamuelFVG2 <felipegoncalvesbr2@gmail.com>
2025-10-31 22:13:19 -0300

    feat: add components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./Button/Button";
+export { default as Footer } from "./Footer/Footer";
+export { default as Header } from "./Header/Header";
+export { default as Input } from "./Input/Input";
+export { default as Select } from "./Select/Select";
+export { default as Table } from "./Table/Table";
+export { default as TableRow } from "./TableRow/TableRow";
