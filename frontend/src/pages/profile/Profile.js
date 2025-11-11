c0bccb31520038e0eb2be1dea76e74d756383dbe
SamuelFVG2 <felipegoncalvesbr2@gmail.com>
2025-11-11 13:15:44 -0300

    fix: fix overflow x, add department to profile page

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -1,9 +1,10 @@
-import { use } from "react";
+import { use, useEffect, useState } from "react";
 import { persons } from "../../assets/testValues";
 import styles from "./Profile.module.css";
 import TableRow from "./TableRow";
 
 export default function Employees() {
+  const [department, setDepartment] = useState("");
   const user = JSON.parse(sessionStorage.getItem("user"));
   let dataToShowOnTable = {
     ID: user._id,
@@ -11,9 +12,21 @@ export default function Employees() {
     CPF: user._cpf,
     Cargo: user._role,
     Email: user._Person__email,
+    Departamento: department || "TechNova Global",
   };
+  useEffect(() => {
+    async function fetchdepartment() {
+      const response = await fetch(
+        `http://localhost:8000/department/${user._departmentID}`
+      );
+      const jsonResponse = await response.json();
+      console.log(user._departmentID);
+      setDepartment(jsonResponse.data._name);
+    }
+    fetchdepartment();
+  }, []);
 
-  // dataToShowOnTable = user;
+  console.log(department);
   return (
     <div className={styles.container} style={{ width: "100vw" }}>
       <h1>Perfil</h1>
